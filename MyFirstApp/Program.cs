199652b9b81b52ca3442b66bda12dbfc8c913d0a
Prabin Rag TK <prabinrag2340@gmail.com>
2025-08-03 02:03:34 +0400

    post body reading with query reader

--- MyFirstApp/Program.cs
+++ MyFirstApp/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.AspNetCore.WebUtilities;
+using Microsoft.Extensions.Primitives;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
@@ -6,7 +9,10 @@ var app = builder.Build();
 //    return "This is My first apt.net core project!"; });
 
 app.Run(async (HttpContext context) => {
+    
     string path = context.Request.Path;
+    string method = context.Request.Method;
+
     if (path == "/" || path == "/home")
     {
         context.Response.StatusCode= 200;
@@ -17,16 +23,44 @@ app.Run(async (HttpContext context) => {
         context.Response.StatusCode = 200;
         await context.Response.WriteAsync("You are in context");
     }
-    else if (path == "/product")
+    else if (method== "GET" && path == "/product")
     {
         context.Response.StatusCode = 200;
         if(context.Request.Query.ContainsKey("id") && context.Request.Query.ContainsKey("name"))
         {
             string id = context.Request.Query["id"];
             string name = context.Request.Query["name"];
-            await context.Response.WriteAsync("Yes you selected" + name + "with ID of " + id);
+            await context.Response.WriteAsync("Yes you selected " + name + " with ID of " + id);
+            return;
         }
         await context.Response.WriteAsync("kindly provide the product id and name");
+    }
+    else if (method == "POST" && path == "/product")
+    {
+        context.Response.StatusCode = 200;
+
+        string id = "";
+        string name = "";
+
+        StreamReader reader = new StreamReader(context.Request.Body);
+        string data = await reader.ReadToEndAsync();
+        Dictionary <string, StringValues> dict = QueryHelpers.ParseQuery(data);
+
+        if(dict != null)
+        {
+            if (dict.ContainsKey("id"))
+            {
+                id = dict["id"];
+            }
+            if (dict.ContainsKey("name"))
+            {
+                name = dict["name"];
+            }
+            await context.Response.WriteAsync("The " + id + " you passed with " + name);
+            return;
+        }
+        
+        await context.Response.WriteAsync("The content you send with body is "+ data);
     }
     else
     {
